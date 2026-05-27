b2742d7441cf8c9dc5297989bda9c4b17f55fd1c
Flasha 🎴 <158839660+Flasha-0@users.noreply.github.com>
2026-05-27 18:06:44 +0300

    feat(flasha): integrate OpenCodeService in activation

--- extensions/flasha-code/src/extension.ts
+++ extensions/flasha-code/src/extension.ts
@@ -1,9 +1,11 @@
 import * as vscode from 'vscode';
 import { ChatViewProvider } from './chatViewProvider';
 import { FlashaModeManager } from './modeManager';
+import { OpenCodeService } from './opencodeService';
 
 export function activate(context: vscode.ExtensionContext) {
   const modeManager = new FlashaModeManager();
+  const opencode = OpenCodeService.getInstance(context);
 
   context.subscriptions.push(
     vscode.commands.registerCommand('flasha.chat', () => {
@@ -26,7 +28,11 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.registerWebviewViewProvider('flasha.chat', new ChatViewProvider(context, modeManager))
   );
 
+  opencode.start();
+
   console.log('[Flasha Code] Activated');
 }
 
-export function deactivate() {}
+export function deactivate() {
+  OpenCodeService.getInstance({} as any).stop();
+}
